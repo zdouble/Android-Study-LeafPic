67275b5f8186f0d53d6ee430c1c6bf8908e222a1
zdouble <872715485@qq.com>
2018-08-01 23:28:42 +0800

    toolbar 10%

--- app/src/main/java/com/example/zdouble/leafpic/activitys/MainActivity.java
+++ app/src/main/java/com/example/zdouble/leafpic/activitys/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.zdouble.leafpic.activitys;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.zdouble.leafpic.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
---
+++ app/src/main/java/com/example/zdouble/leafpic/fragments/AlbumsFragment.java
@@ -0,0 +1,20 @@
+package com.example.zdouble.leafpic.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class AlbumsFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
