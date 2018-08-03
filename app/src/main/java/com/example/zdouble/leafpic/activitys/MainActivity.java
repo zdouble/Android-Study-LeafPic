dcbd8f2adb555c5d4e45f45b444e9a3b9d13cbac
zdouble <872715485@qq.com>
2018-08-03 19:02:51 +0800

    toolbar 30%

--- app/src/main/java/com/example/zdouble/leafpic/activitys/MainActivity.java
+++ app/src/main/java/com/example/zdouble/leafpic/activitys/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.zdouble.leafpic.R;
+import com.example.zdouble.leafpic.fragments.AlbumsFragment;
 
 
 public class MainActivity extends AppCompatActivity {
+    private AlbumsFragment albumsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        albumsFragment = new AlbumsFragment();
+        setContentFragment();
+
+
+    }
+
+    private void setContentFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, albumsFragment, AlbumsFragment.TAG)
+                .addToBackStack(null)
+                .commit();
     }
 }
--- app/src/main/java/com/example/zdouble/leafpic/fragments/AlbumsFragment.java
+++ app/src/main/java/com/example/zdouble/leafpic/fragments/AlbumsFragment.java
@@ -1,12 +1,18 @@
 package com.example.zdouble.leafpic.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.example.zdouble.leafpic.R;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+
 public class AlbumsFragment extends Fragment {
+    public static final String TAG = "AlbumsFragment";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,11 @@ public class AlbumsFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.grid_albums, menu);
+        menu.findItem(R.id.search_action).setIcon(new IconicsDrawable(getContext())
+                .icon(GoogleMaterial.Icon.gmd_search)
+                .color(Color.WHITE)
+                .sizeDp(18));
         super.onCreateOptionsMenu(menu, inflater);
     }
 }
